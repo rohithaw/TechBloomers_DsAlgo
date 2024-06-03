74c0f9a58acefbed75dfe4c2c2287cec5468804e
jayabb258 <164929036+jayabb258@users.noreply.github.com>
2024-06-03 18:20:36 -0500

    Update Runner.java

--- src/test/java/testRunner/Runner.java
+++ src/test/java/testRunner/Runner.java
@@ -20,7 +20,7 @@ public class Runner extends AbstractTestNGCucumberTests {
  
 
 	@Override
-	@DataProvider(parallel = true)
+	@DataProvider(parallel = false)
 	public Object[][] scenarios() {
 		return super.scenarios();
 	}
